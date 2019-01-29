d43bf7ed867a197eb0c8b29142704b72f032d631
Matthew Morgan <mmorgan55@cnm.edu>
2019-01-28 21:48:15 -0700

    Initial commit

---
+++ app/src/main/java/mmorgan/tutorial/android/a2dgame/GamePanel.java
@@ -0,0 +1,56 @@
+package mmorgan.tutorial.android.a2dgame;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.support.annotation.MainThread;
+import android.view.MotionEvent;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+public class GamePanel extends SurfaceView implements SurfaceHolder.Callback {
+
+  private MainThread thread;
+  public GamePanel(Context context) {
+    super(context);
+    getHolder().addCallback(this);
+    thread = new MainThread(getHolder(), this);
+    setFocusable(true);
+  }
+
+  @Override
+  public void surfaceCreated(SurfaceHolder holder) {
+    thread = new MainThread(getHolder(), this);
+
+    thread.setRunning(true);
+    thread.start();
+  }
+
+  @Override
+  public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+
+  }
+
+  @Override
+  public void surfaceDestroyed(SurfaceHolder holder) {
+    boolean retry = true;
+    while (true) {
+      try {
+        thread.setRunning(false);
+        thread.join();
+      } catch (Exception ex) {
+        ex.printStackTrace();
+      }
+      retry = false;
+    }
+  }
+
+  @Override
+  public boolean onTouchEvent(MotionEvent event){
+    return super.onTouchEvent(event);
+  }
+
+  @Override
+  public void draw(Canvas canvas) {
+    super.draw(canvas);
+  }
+}
---
+++ app/src/main/java/mmorgan/tutorial/android/a2dgame/MainActivity.java
@@ -0,0 +1,19 @@
+package mmorgan.tutorial.android.a2dgame;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+import android.view.WindowManager.LayoutParams;
+
+public class MainActivity extends Activity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    getWindow().setFlags(LayoutParams.FLAG_FULLSCREEN, LayoutParams.FLAG_FULLSCREEN);
+    this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+    setContentView(R.layout.activity_main);
+  }
+}
