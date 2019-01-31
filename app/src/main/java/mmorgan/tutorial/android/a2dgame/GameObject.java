f547e40a8514d7206bacb19e6bf2d8336a63df6d
Matthew Morgan <mmorgan55@cnm.edu>
2019-01-30 22:45:37 -0700

    Created GameObject interface and shell for Player class

---
+++ app/src/main/java/mmorgan/tutorial/android/a2dgame/GameObject.java
@@ -0,0 +1,9 @@
+package mmorgan.tutorial.android.a2dgame;
+
+import android.graphics.Canvas;
+
+public interface GameObject {
+  public void draw(Canvas canvas);
+  public void update();
+
+}
---
+++ app/src/main/java/mmorgan/tutorial/android/a2dgame/Player.java
@@ -0,0 +1,16 @@
+package mmorgan.tutorial.android.a2dgame;
+
+import android.graphics.Canvas;
+
+public class Player implements GameObject {
+
+  @Override
+  public void draw(Canvas canvas) {
+    
+  }
+
+  @Override
+  public void update() {
+
+  }
+}
