75fbfa16efdfeb5764da0831d3f09851236d1e21
Matthew Morgan <mmorgan55@cnm.edu>
2019-01-31 22:52:54 -0700

    Created Constants class for constants and added DisplayMetrics to MainActivity

---
+++ app/src/main/java/mmorgan/tutorial/android/a2dgame/Constants.java
@@ -0,0 +1,6 @@
+package mmorgan.tutorial.android.a2dgame;
+
+public class Constants {
+  public static int SCREEN_WIDTH;
+  public static int SCREEN_HEIGHT;
+}
--- app/src/main/java/mmorgan/tutorial/android/a2dgame/MainActivity.java
+++ app/src/main/java/mmorgan/tutorial/android/a2dgame/MainActivity.java
@@ -3,6 +3,8 @@ package mmorgan.tutorial.android.a2dgame;
 import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.view.Display;
 import android.view.Window;
 import android.view.WindowManager;
 import android.view.WindowManager.LayoutParams;
@@ -14,6 +16,12 @@ public class MainActivity extends Activity {
     super.onCreate(savedInstanceState);
     getWindow().setFlags(LayoutParams.FLAG_FULLSCREEN, LayoutParams.FLAG_FULLSCREEN);
     this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+    DisplayMetrics dm = new DisplayMetrics();
+    getWindowManager().getDefaultDisplay().getMetrics(dm);
+    Constants.SCREEN_WIDTH = dm.widthPixels;
+    Constants.SCREEN_HEIGHT = dm.heightPixels;
+
     setContentView(new GamePanel(this));
   }
 }
