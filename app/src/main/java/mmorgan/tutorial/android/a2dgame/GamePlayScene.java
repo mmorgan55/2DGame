d174cf9bc31c38efbb6652e3bddc203ef3b45592
Matthew Morgan <mmorgan55@cnm.edu>
2019-02-09 18:44:26 -0700

    Added Scene interface, SceneManager class, and GamePlayScene class

---
+++ app/src/main/java/mmorgan/tutorial/android/a2dgame/GamePlayScene.java
@@ -0,0 +1,25 @@
+package mmorgan.tutorial.android.a2dgame;
+
+import android.graphics.Canvas;
+
+public class GamePlayScene implements Scene {
+  private SceneManager manager;
+
+  public GamePlayScene(SceneManager manager) {
+    this.manager = manager;
+  }
+  @Override
+  public void update() {
+
+  }
+
+  @Override
+  public void draw(Canvas canvas) {
+
+  }
+
+  @Override
+  public void terminate() {
+
+  }
+}
---
+++ app/src/main/java/mmorgan/tutorial/android/a2dgame/SceneManager.java
@@ -0,0 +1,10 @@
+package mmorgan.tutorial.android.a2dgame;
+
+import java.util.ArrayList;
+
+public class SceneManager {
+  private ArrayList<Scene> scenes = new ArrayList<>();
+  private int activeScene;
+
+
+}
