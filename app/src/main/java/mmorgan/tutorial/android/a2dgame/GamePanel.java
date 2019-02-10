b6e06a01599cf009b069aebc5360d6a171b61ccd
Matthew Morgan <mmorgan55@cnm.edu>
2019-02-09 19:04:36 -0700

    Moved game code from GamePanel to GamePlayScene and added SceneManager variable to GamePanel.
    This was done to clean up the GamePanel class and also for modularity if different types of scenes were to be added later on.

--- app/src/main/java/mmorgan/tutorial/android/a2dgame/GamePanel.java
+++ app/src/main/java/mmorgan/tutorial/android/a2dgame/GamePanel.java
@@ -2,10 +2,6 @@ package mmorgan.tutorial.android.a2dgame;
 
 import android.content.Context;
 import android.graphics.Canvas;
-import android.graphics.Color;
-import android.graphics.Paint;
-import android.graphics.Point;
-import android.graphics.Rect;
 import android.view.MotionEvent;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
@@ -13,31 +9,16 @@ import android.view.SurfaceView;
 public class GamePanel extends SurfaceView implements SurfaceHolder.Callback {
 
   private MainThread thread;
-  private Rect r = new Rect();
-  private Player player;
-  private Point playerPoint;
-  private ObstacleManager obstacleManager;
-  private boolean movingPlayer;
-  private boolean gameOver;
-  private long gameOverTime;
+  private SceneManager manager;
+
 
   public GamePanel(Context context) {
     super(context);
     getHolder().addCallback(this);
     thread = new MainThread(getHolder(), this);
-    player = new Player(new Rect(100, 100, 200, 200), Color.rgb(0, 0, 255));
-    playerPoint = new Point(Constants.SCREEN_WIDTH /2, 3 * Constants.SCREEN_HEIGHT/4);
-    player.update(playerPoint);
-    obstacleManager = new ObstacleManager(200, 350, 75, Color.BLACK);
+    manager = new SceneManager();
 
     setFocusable(true);
-  }
-
-  public void reset() {
-    playerPoint = new Point(Constants.SCREEN_WIDTH /2, 3 * Constants.SCREEN_HEIGHT/4);
-    player.update(playerPoint);
-    obstacleManager = new ObstacleManager(200, 350, 75, Color.BLACK);
-    movingPlayer = false;
   }
 
   @Override
@@ -67,64 +48,20 @@ public class GamePanel extends SurfaceView implements SurfaceHolder.Callback {
 
   @Override
   public boolean onTouchEvent(MotionEvent event) {
-    switch (event.getAction()) {
-      case MotionEvent.ACTION_DOWN:
-        if (!gameOver && player.getRectangle().contains((int) event.getX(), (int) event.getY())) {
-          movingPlayer = true;
-        }
-        if(gameOver && System.currentTimeMillis() - gameOverTime >= 2000) {
-          reset();
-          gameOver = false;
-        }
-        break;
-      case MotionEvent.ACTION_MOVE:
-        if (!gameOver && movingPlayer) {
-          playerPoint.set((int) event.getX(), (int) event.getY());
-        }
-        break;
-      case MotionEvent.ACTION_UP:
-        movingPlayer = false;
-        break;
-    }
-
+    manager.recieveTouch(event);
     return true;
   }
 
   @Override
   public void draw(Canvas canvas) {
     super.draw(canvas);
-    canvas.drawColor(Color.WHITE);
-    player.draw(canvas);
-    obstacleManager.draw(canvas);
 
-    if (gameOver) {
-      Paint paint = new Paint();
-      paint.setTextSize(100);
-      paint.setColor(Color.MAGENTA);
-      drawCenterText(canvas, paint, "Game Over");
-    }
+    manager.draw(canvas);
   }
 
   public void update() {
-    if(!gameOver) {
-      player.update(playerPoint);
-      obstacleManager.update();
-      if(obstacleManager.playerCollide(player)) {
-        gameOver = true;
-        gameOverTime = System.currentTimeMillis();
-      }
-    }
+    manager.update();
   }
 
-  private void drawCenterText(Canvas canvas, Paint paint, String text) {
-    paint.setTextAlign(Paint.Align.LEFT);
-    canvas.getClipBounds(r);
-    int cHeight = r.height();
-    int cWidth = r.width();
-    paint.getTextBounds(text, 0, text.length(), r);
-    float x = cWidth / 2f - r.width() / 2f - r.left;
-    float y = cHeight / 2f + r.height() / 2f - r.bottom;
-    canvas.drawText(text, x, y, paint);
-  }
 
 }
--- app/src/main/java/mmorgan/tutorial/android/a2dgame/GamePlayScene.java
+++ app/src/main/java/mmorgan/tutorial/android/a2dgame/GamePlayScene.java
@@ -1,19 +1,53 @@
 package mmorgan.tutorial.android.a2dgame;
 
 import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Point;
+import android.graphics.Rect;
 import android.view.MotionEvent;
 
 public class GamePlayScene implements Scene {
-  private SceneManager manager;
+
+  private Rect r = new Rect();
+  private Player player;
+  private Point playerPoint;
+  private ObstacleManager obstacleManager;
+  private boolean movingPlayer;
+  private boolean gameOver;
+  private long gameOverTime;
+
+  public GamePlayScene() {
+    player = new Player(new Rect(100, 100, 200, 200), Color.rgb(0, 0, 255));
+    playerPoint = new Point(Constants.SCREEN_WIDTH / 2, 3 * Constants.SCREEN_HEIGHT / 4);
+    player.update(playerPoint);
+    obstacleManager = new ObstacleManager(200, 350, 75, Color.BLACK);
+  }
 
   @Override
   public void update() {
-
+    if (!gameOver) {
+      player.update(playerPoint);
+      obstacleManager.update();
+      if (obstacleManager.playerCollide(player)) {
+        gameOver = true;
+        gameOverTime = System.currentTimeMillis();
+      }
+    }
   }
 
   @Override
   public void draw(Canvas canvas) {
+    canvas.drawColor(Color.WHITE);
+    player.draw(canvas);
+    obstacleManager.draw(canvas);
 
+    if (gameOver) {
+      Paint paint = new Paint();
+      paint.setTextSize(100);
+      paint.setColor(Color.MAGENTA);
+      drawCenterText(canvas, paint, "Game Over");
+    }
   }
 
   @Override
@@ -23,6 +57,43 @@ public class GamePlayScene implements Scene {
 
   @Override
   public void receiveTouch(MotionEvent event) {
+    switch (event.getAction()) {
+      case MotionEvent.ACTION_DOWN:
+        if (!gameOver && player.getRectangle().contains((int) event.getX(), (int) event.getY())) {
+          movingPlayer = true;
+        }
+        if (gameOver && System.currentTimeMillis() - gameOverTime >= 2000) {
+          reset();
+          gameOver = false;
+        }
+        break;
+      case MotionEvent.ACTION_MOVE:
+        if (!gameOver && movingPlayer) {
+          playerPoint.set((int) event.getX(), (int) event.getY());
+        }
+        break;
+      case MotionEvent.ACTION_UP:
+        movingPlayer = false;
+        break;
+    }
+  }
 
+  private void drawCenterText(Canvas canvas, Paint paint, String text) {
+    paint.setTextAlign(Paint.Align.LEFT);
+    canvas.getClipBounds(r);
+    int cHeight = r.height();
+    int cWidth = r.width();
+    paint.getTextBounds(text, 0, text.length(), r);
+    float x = cWidth / 2f - r.width() / 2f - r.left;
+    float y = cHeight / 2f + r.height() / 2f - r.bottom;
+    canvas.drawText(text, x, y, paint);
+  }
+
+
+  public void reset() {
+    playerPoint = new Point(Constants.SCREEN_WIDTH / 2, 3 * Constants.SCREEN_HEIGHT / 4);
+    player.update(playerPoint);
+    obstacleManager = new ObstacleManager(200, 350, 75, Color.BLACK);
+    movingPlayer = false;
   }
 }
